0305de92903224819fbb456c6c558edbf724d453
Andreas Lif <hello@andreaslif.com>
2023-08-15 14:37:00 +0200

    Add base project.

---
+++ Breathe/BreatheApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreatheApp.swift
+//  Breathe
+//
+//  Created by Andreas Lif on 2023-08-15.
+//
+
+import SwiftUI
+
+@main
+struct BreatheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
